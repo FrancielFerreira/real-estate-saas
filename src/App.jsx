1d032ea12facbdc0f5e72769fb4946ab4962a5c0
Franciel Ferreira <franciel.ferreira357@gmail.com>
2023-12-08 03:38:15 -0300

    config router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// reaproveitar components routers v6 com o Outlet
+import { Outlet } from 'react-router-dom';
 
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// pages
-import Home from './pages/Home/Home';
-import Aluguel from './pages/Aluguel/Aluguel';
-import Compra from './pages/Compra/Compra';
-import Venda from './pages/Venda/Venda';
-import Imoveis from './pages/Imoveis/Imoveis';
-import Page404 from './pages/Page404/Page404';
-
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/aluguel/*" element={<Aluguel />} />
-          <Route path="/compra/*" element={<Compra />} />
-          <Route path="/venda/*" element={<Venda />} />
-          <Route path="/imoveis/*" element={<Imoveis />} />
-          <Route path="*" element={<Page404 />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <main style={{ marginTop: '120px' }}>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// pages
+import Home from './pages/Home/Home.jsx';
+import Page404 from './pages/Page404/Page404.jsx';
+import Aluguel from './pages/Aluguel/Aluguel.jsx';
+import Compra from './pages/Compra/Compra.jsx';
+import Venda from './pages/Venda/Venda.jsx';
+import Imoveis from './pages/Imoveis/Imoveis.jsx';
 import App from './App.jsx';
+
+// routers v6
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const routers = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Page404 />, // página de erro 404
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/compra',
+        element: <Compra />,
+      },
+      {
+        path: '/aluguel',
+        element: <Aluguel />,
+      },
+      {
+        path: '/venda',
+        element: <Venda />,
+      },
+      {
+        path: '/imoveis/*',
+        element: <Imoveis />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routers} />
   </React.StrictMode>,
 );
